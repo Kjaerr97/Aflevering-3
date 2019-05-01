404ddd223d611189952fba109c8d726087b5af5a
Asger <Asgerakerman@gmail.com>
2019-05-01 12:07:29 +0200

    ja
    ja

--- Database aflevering/src/Database.java
+++ Database aflevering/src/Database.java
@@ -2,14 +2,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Database implements IGameDAO {
+public class Database implements databaseDAO {
 
     private Connection createConnection() throws SQLException {
         return DriverManager.getConnection("database URL",
                 "username", "password"); // indsæt egne værdier her
 
     }
+
     
+
+
+
 
 
 
---
+++ Database aflevering/src/databaseDAO.java
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+public interface databaseDAO {
+
+    void createRecipe();
+
+}
