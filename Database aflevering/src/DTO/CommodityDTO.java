92a7c878c5e5e8f30f76354753f7a9f6f2d8aeb1
andreaspat <s185034@win.dtu.dk>
2019-05-01 13:46:47 +0200

    CommodityBatch

---
+++ Database aflevering/src/DTO/CommodityDTO.java
@@ -0,0 +1,45 @@
+package DTO;
+
+public class CommodityDTO implements ICommodityDTO {
+
+
+    @Override
+    public int getCommodity_ID() {
+        return 0;
+    }
+
+    @Override
+    public void setCommodity_ID(int commodity_ID) {
+
+    }
+
+    @Override
+    public int getProduct_ID() {
+        return 0;
+    }
+
+    @Override
+    public void setProduct_ID(int Product_ID) {
+
+    }
+
+    @Override
+    public int getIngrediens() {
+        return 0;
+    }
+
+    @Override
+    public void setIngrediens_ID(int ingrediens_id) {
+
+    }
+
+    @Override
+    public int getCommidityBatch_ID() {
+        return 0;
+    }
+
+    @Override
+    public void setCommodityBatch_ID(int commodityBatch_id) {
+
+    }
+}
