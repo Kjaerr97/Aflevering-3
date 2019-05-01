07b470bec51b6f52c4e2ed7192ccf8a707884cb8
Asger <Asgerakerman@gmail.com>
2019-05-01 13:40:31 +0200

    Prøver at comitte første DTO set
    Prøver at comitte første DTO set

---
+++ Database aflevering/src/DTO/IrecipeDTO.java
@@ -0,0 +1,19 @@
+package DTO;
+
+public interface IrecipeDTO {
+
+    int getIngredientsID();
+
+    void setIngredientsID(int ingredientsID);
+
+    int getProductID();
+
+    void setProductID(int productID);
+
+    String getDato();
+
+    void setDato(String Dato);
+
+
+
+}
--- Database aflevering/src/DTO/RecipeDTO.java
+++ Database aflevering/src/DTO/RecipeDTO.java
@@ -1,32 +1,40 @@
 package DTO;
 
 
-public class RecipeDTO {
+public class RecipeDTO implements IrecipeDTO {
     private int ingredientsID;
     private int productID;
     private String dato;
 
+    @Override
     public int getIngredientsID() {
         return ingredientsID;
     }
 
+    @Override
     public void setIngredientsID(int ingredientsID) {
         this.ingredientsID = ingredientsID;
+
     }
 
+    @Override
     public int getProductID() {
         return productID;
     }
 
+    @Override
     public void setProductID(int productID) {
         this.productID = productID;
-    }
 
+    }
+    @Override
     public String getDato() {
         return dato;
     }
 
-    public void setDato(String dato) {
+    @Override
+    public void setDato(String Dato) {
         this.dato = dato;
     }
 }
+
