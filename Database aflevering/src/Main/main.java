2f74ba583bb7a47c8c22dcfa58be8795ee516d4b
Andreas Hyldahl <Andreas.hyldahl10@hotmail.com>
2019-05-06 14:23:20 +0200

    fixed SQL names
    fixed SQL names

--- Database aflevering/src/Main/main.java
+++ Database aflevering/src/Main/main.java
@@ -9,7 +9,7 @@ public class main {
         CommoditybatchDTO commoditybatchDTO = new CommoditybatchDTO();
         commoditybatchDTO.setCommodityBatchID(45);
         commoditybatchDTO.setAmount(234);
-        
+
         databaseDAO.createCommoditybatch(commoditybatchDTO);
     }
 }
